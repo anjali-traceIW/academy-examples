107db422791b6259be4bcfcfcfca039fd5e16d46
anjali-traceIW <55280077+anjali-traceIW@users.noreply.github.com>
2019-09-13 16:09:14 +0100

    Added a cow telling us it's a cow.

--- inheritance.py
+++ inheritance.py
@@ -1,18 +1,28 @@
+import os
+
 class Animal:
     def what_am_i(self):
         return "I am an animal"
+
 class Dog(Animal):
     def what_am_i(self):
         print(Animal.what_am_i(self))
         return "I am a dog"
+
+class Cow(Animal):
+    def what_am_i(self):
+        print(Animal.what_am_i(self))
+        return "I am a cow"
+
+cow = Cow()
 dog = Dog()
-cat = Animal()
 animal = Animal()
 
 print("THIS IS DOG")
 print(dog.what_am_i()) #"I am a dog"
 
 print("\n\nTHIS IS ANIMAL")
-print(animal.what_am_i()) #"I am an animal")
-
-print("Henry is really cool...")
+print(animal.what_am_i()) #"I am an animal"
+						
+print("\n\nTHIS IS COW")
+os.system(f"cowsay {cow.what_am_i()}") #"I am a cow" said the cow
